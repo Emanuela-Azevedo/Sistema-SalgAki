ce162ea7ff6c508d5e39f98617eb5485a1733921
Luciana <luciana02332@gmail.com>
2026-04-02 12:57:27 +0000

    Migração completa do projeto para Spring Boot

---
+++ src/main/java/com/salgaki/SistemaSalgAkiApplication.java
@@ -0,0 +1,13 @@
+package com.salgaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSalgAkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSalgAkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salgaki/SistemaSalgAkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salgaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSalgAkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
